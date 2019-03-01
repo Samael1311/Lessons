db9a61643318737b0196f046e769782eac931152
Rimma Gruzdeva <testStudy1311@gmail.com>
2019-03-01 14:57:32 +0200

    Исправления ошибок 1.2

--- Основное ДЗ/script.js
+++ Основное ДЗ/script.js
@@ -2,17 +2,22 @@
 
 let money = +prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
-var answer1 = +prompt('Введите обязательную статью расходов в этом месяце!\nВо сколько обойдется?', '');
-var answer2 = +prompt('Введите обязательную статью расходов в этом месяце!\nВо сколько обойдется?', '');
+//var answer1 = +prompt('Введите обязательную статью расходов в этом месяце!\nВо сколько обойдется?', '');
+
 let appData = {
 	budget: money,
 	timeData: time,
-	expenses: {
-	[answer1]: answer2,
-	},
+	expenses: {},
 	income: [],
 	savings: false
 }
+
+let answer1 = prompt('Введите обязательную статью расходов в этом месяце!', ''),
+		answer2 = +prompt('Во сколько обойдется?');
+let answer3 = prompt('Введите обязательную статью расходов в этом месяце!', ''),
+		answer4 = +prompt('Во сколько обойдется?');
+appData.expenses[answer1] = answer2; 
+appData.expenses[answer3] = answer4;
 alert("Ваш бюджет на 1 день составляет " + money/30);
 console.log("Бюджет = " + appData.budget,"\n",
 	"Дата " + appData.timeData,"\n", 
