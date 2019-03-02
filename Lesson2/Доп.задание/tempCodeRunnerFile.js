e60b079c2f2681fe2671a03eef0c22be96cf9e6f
Rimma Gruzdeva <testStudy1311@gmail.com>
2019-03-02 17:56:38 +0200

    Исправлен недочет

---
+++ Lesson2/Доп.задание/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+var f = 60;
+switch (f) {
+    case f<49:
+        console.log('false');
+        break;
+    case f>51:
+        console.log('not true');
+        break;
+    case f=50:
+        console.log('true');
+        break;
+};
--- Доп.задание/script.js
+++ Доп.задание/script.js
@@ -5,7 +5,7 @@ let mult = eval(num.toString().split('').join('*') );
 console.log(mult);
 let pow = mult**3;
 console.log(pow);
-let g= pow.toString().split('').slice(0,2);
+let g= pow.toString().split('').slice(0,2).join('');
 alert("Первые цифры: " + g );
 
 
