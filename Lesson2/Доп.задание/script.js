d3296875e28d038766081d452d02eb109250ea30
Rimma Gruzdeva <testStudy1311@gmail.com>
2019-03-02 07:53:04 +0200

    Дабавлено дополнительное задание

---
+++ Lesson2/Доп.задание/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let week = ["Понедельник", "Вторник", "Среда",
+	"Четверг", "Пятница".italics(), "Суббота".bold(), "Воскресенье".bold()];
+
+	document.write(week.join('</br>'));
+ let arr = [1452,33,548,71457,79,523,12548];
+
+ console.log(arr);
+ for (let i = 0; i < arr.length; i++) {
+	 if (arr[i].toString().startsWith(3) || arr[i].toString().startsWith(7)) {
+		 console.log(arr[i]);
+	 }
+	 
+ }
+
+ 
